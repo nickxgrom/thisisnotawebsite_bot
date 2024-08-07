7c1b1de84130088d8289822fa6ed5de317546f62
Nick Gromyko <nxgr.dev@gmail.com>
2024-08-07 12:25:17 +0500

    add schedule

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const express = require('express'),
     app = express(),
     PORT = process.env.PORT || 8080,
     BOT_TOKEN = `bot${process.env.BOT_TOKEN}`,
-    baseUrl = 'https://api.telegram.org'
+    baseUrl = 'https://api.telegram.org',
+    SCHEDULE_INTERVAL = process.env.SCHEDULE_INTERVAL || 60000
+
+const activeChats = []
 
 app.use(express.json())
 
 async function sendMessage(chatId, message) {
     if (!chatId) return
 
-    const res = await fetch(`${baseUrl}/${BOT_TOKEN}/sendMessage`, {
+    await fetch(`${baseUrl}/${BOT_TOKEN}/sendMessage`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -21,19 +24,52 @@ async function sendMessage(chatId, message) {
             text: message
         })
     })
-
-    console.log(res)
 }
+
+async function getWellIsIt() {
+    const res = await fetch('https://files.thisisnotawebsitedotcom.com/is-it-time-yet/well-is-it.txt')
+
+    return await res.text() !== 'NO'
+}
+
+function setScheduler() {
+    setInterval(async () => {
+        if (activeChats.length > 0) {
+            const wellIsIt = await getWellIsIt()
+
+            activeChats.forEach((chat) => {
+                sendMessage(chat, wellIsIt ? 'something changed. visit https://www.thisisnotawebsitedotcom.com/ password: T.J.Eckleburg' : 'it\'s still not')
+            })
+        }
+    }, SCHEDULE_INTERVAL)
+}
+
+app.get('/',  async (req, res) => {
+    await getWellIsIt()
+
+    res.sendStatus(200)
+} )
+
 
 app.post('/', async (req, res) => {
     const chatId = req.body?.message?.from?.id
-    console.log(req.body)
-    console.log(chatId)
-    await sendMessage(chatId, req.body?.message?.text?.trim() === 'ping' ? 'pong' : 'something went wrong')
+
+    if (req.body?.message?.text?.trim() === 'well is it?') {
+        if (!activeChats.includes(chatId)) {
+            activeChats.push(chatId)
+            console.log(`new chatId was added: ${chatId}`)
+            await sendMessage(chatId, `you have subscribed to updates`)
+        }
+
+        await sendMessage(chatId, `i will notice you status in next hour`)
+    } else {
+        await sendMessage(chatId, 'i dunno what you mean')
+    }
 
     res.sendStatus(200)
 })
 
 app.listen(PORT, () => {
     console.log(`Bot listening ${PORT}`)
+    setScheduler()
 })
